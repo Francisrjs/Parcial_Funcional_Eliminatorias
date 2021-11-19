c4f3a7d47459faeee537e2e0a931379ba5b6b8ca
Francisrjs <47805559+Francisrjs@users.noreply.github.com>
2021-11-19 11:11:38 -0300

    detalles

--- respuesta.hs
+++ respuesta.hs
@@ -7,15 +7,18 @@ data Jugador=UnJugador {nacionalidad::String,cotizacion::Number} deriving (Eq,Sh
 data Entrenador=UnEntrenador {aniosExp::Number} deriving (Eq,Show)
 data Equipo= UnEquipo {entrenador::Entrenador,pais::String,jugadores::[Jugador]}deriving (Eq,Show)
 scaloni= UnEntrenador 30
+entrenadorDefault= UnEntrenador 10
 dybala= UnJugador "Argentina" 3000
 messi= UnJugador "Argentina" 300000
 ney= UnJugador "Brasil" 300000
 marquinhos= UnJugador "Brasil" 300000
 aguero= UnJugador "Argentina" 55000
 diMaria= UnJugador "Argentina" 60000
+jugadorRepetido= UnJugador "China" 6
 argentina= UnEquipo scaloni "Argentina" [messi,dybala,aguero]
-brasil= UnEquipo scaloni "Argentina" [ney,marquinhos]
-colombia= UnEquipo scaloni "Argentina" [ney,marquinhos]
+brasil= UnEquipo scaloni "Brasil" [ney,marquinhos]
+colombia= UnEquipo entrenadorDefault "Colombia" [ney,marquinhos]
+china= UnEquipo entrenadorDefault "China" [jugadorRepetido,jugadorRepetido,jugadorRepetido,jugadorRepetido,jugadorRepetido,jugadorRepetido]
 cantJugadoresFueraDelPais::Equipo->Number
 cantJugadoresFueraDelPais equipo= (length.(filter(\jugador-> (nacionalidad jugador) == (pais equipo) )).jugadores) equipo
 cotizacionJugadores::Equipo->Number
@@ -28,7 +31,7 @@ agregarJugador :: Equipo -> Jugador -> Equipo
 agregarJugador equipo jugadorNuevo=equipo{jugadores= [jugadorNuevo]++(jugadores equipo)}
 quitarUltimoJugador::Equipo->Equipo
 quitarUltimoJugador equipo=equipo {jugadores= filter ((last(jugadores equipo))/=) (jugadores equipo)}
---PARTE 2, PARTIDOS
+--PARTE 2, PARTIDOS (TIPOS DE ESTRATEGIAS)
 --funcion para aplicar distintos casos
 ganadorPartido funcion equipo1 equipo2
     |funcion equipo1 > funcion equipo2 =equipo1
@@ -50,7 +53,7 @@ entrenadorExperimentado eq1 eq2= ganadorPartido (aniosExp.entrenador) eq1 eq2
 fueraDelPais :: Equipo -> Equipo -> Equipo
 fueraDelPais eq1 eq2=ganadorPartido (cantJugadoresFueraDelPais) eq1 eq2
 
---Parte 3, TORNEOS
+--Parte 3, TIPOS DE TORNEOS
 type Estrategia= Equipo->Equipo->Equipo
 --Potrero
 equiposP=[argentina,brasil]
@@ -63,14 +66,31 @@ sinElCandidato equipos candidato= filter (candidato /=) equipos
 leGanoAlCandidato estrategia candidato equipos= head((filter(not.ganaElPartido estrategia candidato) equipos))
 leGanoATodos ::(Equipo -> Equipo -> Equipo) -> [Equipo] -> Equipo -> Bool
 leGanoATodos estrategia equipos equipo= all (ganaElPartido estrategia equipo) (sinElCandidato equipos equipo)
+candidato ::(Equipo -> Equipo -> Equipo) -> Equipo -> [Equipo] -> Equipo
 candidato estrategia ayudador equipos
     |leGanoATodos estrategia equipos ayudador=ayudador
     |otherwise=leGanoAlCandidato estrategia ayudador equipos
+invicto :: (Equipo -> Equipo -> Equipo) -> [Equipo] -> Equipo
 invicto estrategia equipos
     |any(leGanoATodos estrategia equipos) equipos=head (filter(leGanoATodos estrategia equipos) equipos)
     |otherwise= head equipos
+--EJEMPLOS:
+--1ER EJEMPLO,Quien gana un torneo de potrero, en el que gana el que mas jugadores tiene que juegan en china, 
+--participando argentina, uruguay, colombia y brasil?
 
---PARTE 4 - ELIMINACION DIRECTA 
+--en consola:potrero [argentina,brasil,colombia,china] (masJugadoresEnPaisDado "china")
+--r:UnEquipo {entrenador = UnEntrenador {aniosExp = 10}, pais = "China", jugadores = [UnJugador {nacionalidad = "China", cotizacion = 6},UnJugador {nacionalidad = "China", cotizacion = 6},UnJugador {nacionalidad = "China", cotizacion = 6},UnJugador {nacionalidad = "China", cotizacion = 6},UnJugador {nacionalidad = "China", cotizacion = 6},UnJugador {nacionalidad = "China", cotizacion = 6}]}
+
+
+--2 EJEMPLO,Quien gana un torneo con candidato, donde el candidato es brasil, los partidos se ganan por mayor cotizacion de sus jugadores
+--y juegan argentina, uruguay, colombia, ecuador y brasil?
+--en consola: *Library> candidato mejorCotizacion brasil equiposP
+--r:UnEquipo {entrenador = UnEntrenador {aniosExp = 30}, pais = "Argentina", jugadores = [UnJugador {nacionalidad = "Argentina", cotizacion = 300000},UnJugador {nacionalidad = "Argentina", cotizacion = 3000},UnJugador {nacionalidad = "Argentina", cotizacion = 55000}]}
+
+--3 ejemplo,Quién gana un torneo en formato invicto, jugando con la estrategia inventada, con un cierto conjunto de equipos?
+--en consola:*Library> invicto fueraDelPais equiposP
+--r:UnEquipo {entrenador = UnEntrenador {aniosExp = 30}, pais = "Brasil", jugadores = [UnJugador {nacionalidad = "Brasil", cotizacion = 300000},UnJugador {nacionalidad = "Brasil", cotizacion = 300000}]}
+--PARTE 4 - ELIMINACION DIRECTA  (FASE DE GRUPOPS)
 torneo estrategia []= []
 torneo estrategia (equipo:[])=[equipo]
 torneo estrategia (pEquipo:sEquipo:rEquipos)=torneo estrategia ([estrategia pEquipo sEquipo]++(torneo estrategia rEquipos))
@@ -79,7 +99,9 @@ torneo estrategia (pEquipo:sEquipo:rEquipos)=torneo estrategia ([estrategia pEqu
 --Averiguar que pasaría si todos los equipos participantes pierden al ultimo jugador en llegar 
 -- ¿puede cambiar el ganador de un torneo? en caso afirmativo, mostrar un ejemplo donde suceda. En caso contrario explicar por qué no.
 --RESPUESTA: cambia el resultado del torneo si la estrategia utilizada depende de todos los jugadores
+--una estrategia que podría suceder en a estrategia de la cantidad de jugadoores fuera de pais o en su pais
 
 --Justificar. ¿que pasaría si un equipo tiene infinitos jugadores?
---debido que todas las estrategias depende de los jugadores y todas las funciones que tengo son finitas
---es imposible saber cual es el ganador ya que haskell sera iomposible computarlo
+--Debido que las mayorias las estrategias depende de los jugadores y todas las funciones que tengo son finitas
+--es imposible saber cual es el ganador ya que haskell sera iomposible computarlo.
+--Pero se podría ganar el torneo si y solo si con el entrenador experimentado , mientras que las otras estrategias no podría suceder ya que es infinito
